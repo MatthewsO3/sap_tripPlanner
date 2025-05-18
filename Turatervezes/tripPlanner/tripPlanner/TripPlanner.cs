a810274335488683f39f9897bdae7720f00daf43
Czap Máté <mate.czap@edu.bme.hu>
2025-05-18 10:59:28 +0200

    bc

--- Turatervezes/tripPlanner/tripPlanner/TripPlanner.cs
+++ Turatervezes/tripPlanner/tripPlanner/TripPlanner.cs
@@ -2,6 +2,7 @@
 using SAPbouiCOM;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Windows.Forms;
 using tripPlanner.Services.Form;
@@ -118,7 +119,7 @@ namespace tripPlanner
                     {
                     }
 
-                    using (var formService = new FormService("turatervezo_form.xml"))
+                   using (var formService = new FormService("turatervezo_form.xml"))
                     {
                         string formXML = formService.ExportToString(new Dictionary<string, string>
                         {
@@ -169,88 +170,7 @@ namespace tripPlanner
                         {
                             try
                             {
-                                string rendszam = oForm.DataSources.UserDataSources.Item("UD_0").Value;
-                                string szallKor = oForm.DataSources.UserDataSources.Item("UD_1").Value;
-                                string szallDatum = oForm.DataSources.UserDataSources.Item("UD_2").Value;
-                                string rendszamParam = string.IsNullOrEmpty(rendszam) ? "NULL" : $"N'{rendszam.Replace("'", "''")}'";
-                                string szallKorParam = string.IsNullOrEmpty(szallKor) ? "NULL" : $"N'{szallKor.Replace("'", "''")}'";
-                                string szallDatumParam = string.IsNullOrEmpty(szallDatum) ? "NULL" : $"N'{szallDatum.Replace("'", "''")}'";
-
-                                SAPbouiCOM.DataTable oDT;
-                                oDT = oForm.DataSources.DataTables.Item("DT_0");
-                                oDT.Rows.Clear();
-
-
-                                string query = $"EXEC NTT_TURATERVEZES @rendszam = {rendszamParam}, @szallKor = {szallKorParam}, @szallDatum = {szallDatumParam}";
-                                var oRs = (SAPbobsCOM.Recordset)globalD.oCompany.GetBusinessObject(BoObjectTypes.BoRecordset);
-                                SAPbouiCOM.DataTable dt = oForm.DataSources.DataTables.Item("DT_0");
-                                oRs.DoQuery(query);
-
-                                if (oRs.RecordCount == 0)
-                                {
-                                    SBO_Application.StatusBar.SetText("Nincs találat!", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
-                                }
-                                else
-                                {
-                                    docEntries = new int[oRs.RecordCount];
-                                    oDT.Rows.Clear();
-                                    for (int i = 0; i < oRs.RecordCount; i++)
-                                    {
-                                        oDT.Rows.Add();
-                                        docEntries[i] = oRs.Fields.Item("docEntry").Value;
-                                        oDT.Columns.Item("db_ordid").Cells.Item(i).Value = oRs.Fields.Item("order_id").Value;
-
-                                        oDT.Columns.Item("db_date").Cells.Item(i).Value = oRs.Fields.Item("del_date").Value;
-
-                                        oDT.Columns.Item("db_lic").Cells.Item(i).Value = oRs.Fields.Item("car_id").Value;
-                                        oDT.Columns.Item("db_driver").Cells.Item(i).Value = oRs.Fields.Item("driver").Value;
-                                        oDT.Columns.Item("db_circle").Cells.Item(i).Value = oRs.Fields.Item("del_circle").Value;
-                                        oDT.Columns.Item("db_ordnm").Cells.Item(i).Value = oRs.Fields.Item("order_name").Value;
-                                        oDT.Columns.Item("db_addr").Cells.Item(i).Value = oRs.Fields.Item("del_addr").Value;
-                                        oDT.Columns.Item("db_ordkg").Cells.Item(i).Value = oRs.Fields.Item("order_kg").Value;
-                                        oDT.Columns.Item("db_tourkg").Cells.Item(i).Value = oRs.Fields.Item("tour_kg").Value;
-                                        oDT.Columns.Item("db_descor").Cells.Item(i).Value = oRs.Fields.Item("desc_ord").Value;
-                                        oDT.Columns.Item("db_descad").Cells.Item(i).Value = oRs.Fields.Item("desc_addr").Value;
-                                        oDT.Columns.Item("db_days").Cells.Item(i).Value = oRs.Fields.Item("days").Value;
-                                        oDT.Columns.Item("db_carkg").Cells.Item(i).Value = oRs.Fields.Item("car_kg").Value;
-                                        oRs.MoveNext();
-                                        
-                                        
-                                    }
-                                    SAPbouiCOM.Item gridItem = oForm.Items.Item("Item_7");
-                                    SAPbouiCOM.Matrix matrix = (SAPbouiCOM.Matrix)gridItem.Specific;
-                                    matrix.AutoResizeColumns();
-
-
-                                    int red = (255 & 0xFF) | ((80 & 0xFF) << 8) | ((80 & 0xFF) << 16);
-                                    for (int i = 0; i < oRs.RecordCount; i++)
-                                    {
-                                        /*decimal tour;
-                                        string tourStr = oDT.Columns.Item("db_tourkg").Cells.Item(i).Value?.ToString();
-                                        if (!decimal.TryParse(tourStr, out tour))
-                                        {
-                                            tour = 1100; // Alapértelmezett érték, ha nem sikerül konvertálni
-                                            SBO_Application.StatusBar.SetText($"Érvénytelen érték a(z) {i + 1}. sorban a tour_kg oszlopban", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
-                                        }*/
-
-                                        decimal cap;
-                                        string capStr = oDT.Columns.Item("db_carkg").Cells.Item(i).Value?.ToString();
-                                        if (!decimal.TryParse(capStr, out cap))
-                                        {
-                                            cap = 1000; // Alapértelmezett érték, ha nem sikerül konvertálni
-                                            SBO_Application.StatusBar.SetText($"Érvénytelen érték a(z) {i + 1}. sorban a car_kg oszlopban", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
-                                        }
-                                        if (oDT.Columns.Item("db_tourkg").Cells.Item(i).Value >= cap) {
-                                            
-                                            matrix.CommonSetting.SetRowBackColor(i, red);
-                                            SBO_Application.StatusBar.SetText($"CommonSettng hiba, {oDT.Rows.Count} rows loaded", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Success);
-
-                                        }
-                                    }
-
-                                    matrix.LoadFromDataSource();
-                                    SBO_Application.StatusBar.SetText($"Query executed, {oDT.Rows.Count} rows loaded", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Success);
-                                }
+                                LoadMatrix(oForm);
                             }
                             catch (Exception ex)
                             {
@@ -259,25 +179,28 @@ namespace tripPlanner
                         }
                         break;
                     case BoEventTypes.et_MATRIX_LINK_PRESSED:
-                        {
-                            try
+                            if (pVal.FormTypeEx == "60004" && !pVal.BeforeAction)
                             {
-                                if(pVal.ColUID == "order_id")
+                                try
                                 {
-                                    var oItem = oForm.Items.Item("Item_7");
-                                    var oMatrix = oItem.Specific as SAPbouiCOM.Matrix;
-                                    var ordEditText = oMatrix.GetCellSpecific("order_id", pVal.Row) as EditText;
-                                    var ord = ordEditText.Value;
-                                    SBO_Application.OpenForm(BoFormObjectEnum.fo_Order, "", ord);
+                                    if (pVal.ColUID == "order_id")
+                                    {
+                                        var oItem = oForm.Items.Item("Item_7");
+                                        var oMatrix = oItem.Specific as SAPbouiCOM.Matrix;
+                                        var ordEditText = oMatrix.GetCellSpecific("order_id", pVal.Row) as EditText;
+                                        var ord = ordEditText.Value;
+                                        SBO_Application.OpenForm(BoFormObjectEnum.fo_Order, "", docEntries[pVal.Row - 1].ToString());
+                                    }
                                 }
-                            }
-                            catch(Exception ex)
-                            {
-                                SBO_Application.StatusBar.SetText("Error linked button" + ex.Message + " | Stack: " + ex.StackTrace, BoMessageTime.bmt_Long, BoStatusBarMessageType.smt_Error);
+                                catch (Exception ex)
+                                {
+                                    SBO_Application.StatusBar.SetText("Error linked button" + ex.Message + " | Stack: " + ex.StackTrace, BoMessageTime.bmt_Long, BoStatusBarMessageType.smt_Error);
 
+                                }
+                                
                             }
-                            break;
-                        }
+                        break;
+
                 }
                
             }
@@ -413,6 +336,103 @@ namespace tripPlanner
 
 
         }
+        
+        private void checkIfOverLimit(DataTable oDT, Matrix matrix)
+        {
+            int red = (255 & 0xFF) | ((80 & 0xFF) << 8) | ((80 & 0xFF) << 16);
+
+            for (int i = 0; i < oDT.Rows.Count; i++)
+            {
+                decimal cap, tour;
+                string capStr = oDT.Columns.Item("db_carkg").Cells.Item(i).Value?.ToString();
+                string tourStr = oDT.Columns.Item("db_tourkg").Cells.Item(i).Value?.ToString();
+
+                if (!decimal.TryParse(capStr, NumberStyles.Float, CultureInfo.InvariantCulture, out cap))
+                {
+
+                    SBO_Application.StatusBar.SetText(
+                        $"Érvénytelen érték a(z) {i + 1}. sorban a car_kg oszlopban",
+                        BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
+                }
+
+                if (!decimal.TryParse(tourStr, NumberStyles.Float, CultureInfo.InvariantCulture, out tour))
+                {
+                    SBO_Application.StatusBar.SetText(
+                        $"Érvénytelen érték a(z) {i + 1}. sorban a tour_kg oszlopban",
+                        BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
+                }
+
+                if (tour >= cap)
+                {
+                    matrix.CommonSetting.SetRowBackColor(i + 1, red);
+                    SBO_Application.StatusBar.SetText(
+                        $"carcap: {cap}, tour: {tour}, rows loaded",
+                        BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Success);
+                }
+
+            }
+        }
+        private void LoadMatrix(SAPbouiCOM.Form oForm)
+        {
+            string rendszam = oForm.DataSources.UserDataSources.Item("UD_0").Value;
+            string szallKor = oForm.DataSources.UserDataSources.Item("UD_1").Value;
+            string szallDatum = oForm.DataSources.UserDataSources.Item("UD_2").Value;
+            string rendszamParam = string.IsNullOrEmpty(rendszam) ? "NULL" : $"N'{rendszam.Replace("'", "''")}'";
+            string szallKorParam = string.IsNullOrEmpty(szallKor) ? "NULL" : $"N'{szallKor.Replace("'", "''")}'";
+            string szallDatumParam = string.IsNullOrEmpty(szallDatum) ? "NULL" : $"N'{szallDatum.Replace("'", "''")}'";
+
+            SAPbouiCOM.DataTable oDT;
+            oDT = oForm.DataSources.DataTables.Item("DT_0");
+            oDT.Rows.Clear();
+
+
+            string query = $"EXEC NTT_TURATERVEZES @rendszam = {rendszamParam}, @szallKor = {szallKorParam}, @szallDatum = {szallDatumParam}";
+            var oRs = (SAPbobsCOM.Recordset)globalD.oCompany.GetBusinessObject(BoObjectTypes.BoRecordset);
+            SAPbouiCOM.DataTable dt = oForm.DataSources.DataTables.Item("DT_0");
+            oRs.DoQuery(query);
+
+            if (oRs.RecordCount == 0)
+            {
+                SBO_Application.StatusBar.SetText("Nincs találat!", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Error);
+            }
+            else
+            {
+                docEntries = new int[oRs.RecordCount];
+                oDT.Rows.Clear();
+                for (int i = 0; i < oRs.RecordCount; i++)
+                {
+                    oDT.Rows.Add();
+                    docEntries[i] = oRs.Fields.Item("docEntry").Value;
+                    oDT.Columns.Item("db_ordid").Cells.Item(i).Value = oRs.Fields.Item("order_id").Value;
+
+                    oDT.Columns.Item("db_date").Cells.Item(i).Value = oRs.Fields.Item("del_date").Value;
+
+                    oDT.Columns.Item("db_lic").Cells.Item(i).Value = oRs.Fields.Item("car_id").Value;
+                    oDT.Columns.Item("db_driver").Cells.Item(i).Value = oRs.Fields.Item("driver").Value;
+                    oDT.Columns.Item("db_circle").Cells.Item(i).Value = oRs.Fields.Item("del_circle").Value;
+                    oDT.Columns.Item("db_ordnm").Cells.Item(i).Value = oRs.Fields.Item("order_name").Value;
+                    oDT.Columns.Item("db_addr").Cells.Item(i).Value = oRs.Fields.Item("del_addr").Value;
+                    oDT.Columns.Item("db_ordkg").Cells.Item(i).Value = oRs.Fields.Item("order_kg").Value;
+                    oDT.Columns.Item("db_tourkg").Cells.Item(i).Value = oRs.Fields.Item("tour_kg").Value;
+                    oDT.Columns.Item("db_descor").Cells.Item(i).Value = oRs.Fields.Item("desc_ord").Value;
+                    oDT.Columns.Item("db_descad").Cells.Item(i).Value = oRs.Fields.Item("desc_addr").Value;
+                    oDT.Columns.Item("db_days").Cells.Item(i).Value = oRs.Fields.Item("days").Value;
+                    oDT.Columns.Item("db_carkg").Cells.Item(i).Value = oRs.Fields.Item("car_kg").Value;
+                    oRs.MoveNext();
+
+
+                }
+                SAPbouiCOM.Item gridItem = oForm.Items.Item("Item_7");
+                SAPbouiCOM.Matrix matrix = (SAPbouiCOM.Matrix)gridItem.Specific;
+                matrix.AutoResizeColumns();
+
+                matrix.LoadFromDataSource();
+                checkIfOverLimit(oDT, matrix);
+
+
+                SBO_Application.StatusBar.SetText($"Query executed, {oDT.Rows.Count} rows loaded", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Success);
+            }
+        }
         private string GetCodeFromName(string tableName, string name)
         {
             Recordset oRs = (Recordset)globalD.oCompany.GetBusinessObject(BoObjectTypes.BoRecordset);
@@ -425,6 +445,5 @@ namespace tripPlanner
             }
             return "";
         }
-
     }
 }
